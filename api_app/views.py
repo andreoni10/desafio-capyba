f579917b22f2dd7faa0d4e5d62038ba2de1a1ac6
Lucas Andreoni <lucasandreoni2000@gmail.com>
2022-05-13 20:52:42 -0300

    Film pagination and filtering

--- api_app/views.py
+++ api_app/views.py
@@ -1,8 +1,7 @@
-from ast import Delete
-from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 import json
+from django.core.paginator import Paginator
 from .models import Films
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -22,30 +21,31 @@ class Film(View):
             'film_genre': f_genre,
         }
 
-        list_film = Films.objects.create(**film_data)
+        film = Films.objects.create(**film_data)
 
         data = {
-            "messege": f"New item added to List with id: {list_film.id}"
+            "messege": f"New item added to List with id: {film.id}"
         }
         return JsonResponse(data, status=201)
 
     def get(self, request):
-        films_count = Films.objects.count()
-        films = Films.objects.all()
-        
+        page_number = request.GET.get('page_number', 1)
+        paginatedFilms = filterAndPaginate(request.GET, Films.objects.all())
         films_data = []
-        for film in films:
+        
+        for film in paginatedFilms.page(page_number).object_list:
             films_data.append({
                 'film_name': film.film_name,
                 'film_release': film.film_release,
                 'film_genre': film.film_genre,
             })
 
-            data = {
-                'films': films_data,
-                'count': films_count,
-            }
-            return JsonResponse(data)
+        data = {
+            'films': films_data,
+            'count': paginatedFilms.count,
+        }
+        
+        return JsonResponse(data)
 
 @method_decorator(csrf_exempt, name='dispatch')
 class FilmUpdate(View):
@@ -68,3 +68,15 @@ class FilmUpdate(View):
             'messege' : f'Film {film_id} has been deleted'
         }
         return JsonResponse(data)
+
+def filterAndPaginate(GET, films):
+    page_size = GET.get('page_size', 5)
+    name = GET.get('name')
+    order_by = GET.get('order_by')
+
+    if(name):
+        films = films.filter(film_name__contains = name)
+
+    films = films.order_by(order_by or 'film_release')
+
+    return Paginator(films, page_size)
