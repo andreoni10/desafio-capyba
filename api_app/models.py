fe0e7c9150b5473c51243f54a000704739a467bf
Lucas Andreoni <lucasandreoni2000@gmail.com>
2022-05-26 23:52:39 -0300

    created user model

--- api_app/models.py
+++ api_app/models.py
@@ -4,3 +4,8 @@ class Films(models.Model):
     film_name = models.CharField(max_length=100)
     film_release = models.IntegerField()
     film_genre = models.CharField(max_length=50)
+
+class User(models.Model):
+    user_name = models.CharField(max_length=50)
+    user_email = models.CharField(max_length=50)
+    user_image_url = models.CharField(max_length=100)
